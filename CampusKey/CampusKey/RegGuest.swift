586d1684038829793dd21b35ec4bb2364e45b4bd
Anthony Lopez <tonylopez92m@gmail.com>
2020-11-04 22:03:03 -0800

    Added guest reg

---
+++ CampusKey/CampusKey/RegGuest.swift
@@ -0,0 +1,43 @@
+//
+//  RegGuest.swift
+//  CampusKey
+//
+//  Created by Anthony Lopez on 11/4/20.
+//
+
+import UIKit
+
+class RegGuest: UIViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var emailHolder: UITextField!
+    
+    @IBOutlet weak var passwordHolder: UITextField!
+    
+    @IBAction func RegisterButton(_ sender: Any) {
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == emailHolder
+            {return emailHolder.resignFirstResponder()}
+        else if textField == passwordHolder
+            {return passwordHolder.resignFirstResponder()}
+        
+        return true
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.emailHolder.delegate = self
+        self.passwordHolder.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
